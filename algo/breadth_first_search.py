6d996ed5101ab4a2e69cf3b361af4eef1480366b
Lisayjn749 <lisayuyjn@gmail.com>
2025-10-10 16:09:10 -0400

    bfs

---
+++ algo/breadth_first_search.py
@@ -0,0 +1,38 @@
+graph = {
+        'A': ['B', 'C'],
+        'B': ['A', 'D', 'E'],
+        'C': ['A', 'F'],
+        'D': ['B'],
+        'E': ['B', 'F'],
+        'F': ['C', 'E']
+    }
+
+def bfs(graph, start_node):
+    """
+    Performs a Breadth-First Search (BFS) on a graph.
+
+    Args:
+        graph (dict): An adjacency list representation of the graph.
+                      Keys are nodes, values are lists of connected nodes.
+        start_node: The starting node for the BFS traversal.
+
+    Returns:
+        list: A list of nodes in the order they were visited during BFS.
+    """
+
+    queue = []
+    queue.append(graph[start_node])
+    visited = [start_node]
+
+    while queue:
+        next = queue.pop(0)
+        for neighbor in graph[next]:
+            if neighbor not in visited:
+                visited.append(neighbor)
+                queue.append(neighbor)
+    return visited
+
+print(bfs(graph, 'A'))           
+            
+
+
