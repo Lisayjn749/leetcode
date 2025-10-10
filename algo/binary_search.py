a74cab65f13605ad090666467d2cf8e573050a30
Lisayjn749 <lisayuyjn@gmail.com>
2025-10-10 15:11:08 -0400

    binary search

---
+++ algo/binary_search.py
@@ -0,0 +1,19 @@
+# leetcode #704
+# nums: list[int], sorted
+# target: int
+# return index if target exists in num, -1 otherwise
+
+def binary_search(nums, target):
+    left = 0
+    right = len(nums) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
